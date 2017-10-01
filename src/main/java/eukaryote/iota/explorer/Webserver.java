f78c2006a2fa04c5f580e3a169b215a64b7c4e44
eukaryote <eukaryote31@users.noreply.github.com>
2017-09-30 21:10:55 -0600

    Use internal ip

--- src/main/java/eukaryote/iota/explorer/Webserver.java
+++ src/main/java/eukaryote/iota/explorer/Webserver.java
@@ -93,7 +93,7 @@ public class Webserver extends NanoHTTPD {
 		dateFormatGmt.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		String[] hosts = {
-				"node.iotasear.ch"
+				"10.128.0.4"
 				};
 
 		api = new IotaAPI.Builder().protocol("http").host(hosts[RandomUtils.nextInt(0, hosts.length)]).port("14265").build();
